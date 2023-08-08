3fbc77dfd6b6ca3d22eb1bc5ae7cf866c8791b21
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-08-08 21:48:43 +0200

    add react-fast-pdf package

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+// TODO: Change the package source in package.json
+// TODO: Use built source code, not initial
+import ReactFastPDF from 'react-fast-pdf/src';
 
 function App() {
-    return <h1>Hello, I am react-fast-pdf!</h1>;
+    return <h1>Hello, I am {ReactFastPDF.PackageName}!</h1>;
 }
 
 export default App;
