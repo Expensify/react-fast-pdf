e249e031bac823396de644d6ed642a08b4502879
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-11-07 14:37:24 +0100

    stop tracking dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import PDFPreviewer from './PDFPreviewer';
-export { PDFPreviewer };
-declare const _default: {
-    PackageName: string;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
