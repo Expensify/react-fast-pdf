0aa97d3d60a9915b89193127dd37acfac4e5b283
Mykhailo Kravchenko <rezkiy37@gmail.com>
2023-11-28 14:04:28 +0100

    integrate file choosing

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties} from 'react';
+import React, {CSSProperties, useState} from 'react';
 import ReactFastPDF, {PDFPreviewer} from 'react-fast-pdf';
 import './index.css';
 
@@ -9,17 +9,58 @@ const pdfPreviewerContainerStyle: CSSProperties = {
     borderStyle: 'solid',
 };
 
+const buttonsContainer: CSSProperties = {
+    marginTop: 16,
+    display: 'flex',
+};
+
+const fileButtonStyle: CSSProperties = {
+    margin: '0 8px',
+    padding: '8px 16px',
+    fontSize: 18,
+    border: 0,
+    borderRadius: 16,
+    backgroundColor: '#03d47c',
+    cursor: 'pointer',
+};
+
 function App() {
+    const [file, setFile] = useState<string | null>(null);
+
     return (
         <main className="container">
             <h1 className="title">Hello, I am {ReactFastPDF.PackageName}!</h1>
 
-            <PDFPreviewer
-                file="/example.pdf"
-                pageMaxWidth={1000}
-                isSmallScreen={false}
-                containerStyle={pdfPreviewerContainerStyle}
-            />
+            {file ? (
+                <PDFPreviewer
+                    file={file}
+                    pageMaxWidth={1000}
+                    isSmallScreen={false}
+                    containerStyle={pdfPreviewerContainerStyle}
+                />
+            ) : (
+                <>
+                    <h3>Please choose a file for previewing:</h3>
+
+                    <div style={buttonsContainer}>
+                        <button
+                            style={fileButtonStyle}
+                            type="button"
+                            onClick={() => setFile('example.pdf')}
+                        >
+                            example.pdf
+                        </button>
+
+                        <button
+                            style={fileButtonStyle}
+                            type="button"
+                            onClick={() => setFile('example_protected.pdf')}
+                        >
+                            example_protected.pdf
+                        </button>
+                    </div>
+                </>
+            )}
         </main>
     );
 }
