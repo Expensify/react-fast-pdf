5e866c780dcad43d4313d21422188fb2b5c4f718
Mykhailo Kravchenko <rezkiy37@gmail.com>
2024-02-01 11:14:01 +0100

    remove dist

--- dist/styles.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ComponentStyles } from './types';
-declare const pdfPreviewerStyles: ComponentStyles;
-declare const pdfPasswordFormStyles: ComponentStyles;
-export { pdfPreviewerStyles, pdfPasswordFormStyles };
-//# sourceMappingURL=styles.d.ts.map
