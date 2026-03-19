35b5e362b8e8333de0331fa37a7d60c4bf76f588
Yauheni Pasiukevich <pasyukevich@live.com>
2026-03-19 21:03:43 +0100

    feat: add ROTATION_DEGREES constant for valid PDF page rotations

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import PDFPreviewer from './PDFPreviewer.js';
+import {ROTATION_DEGREES} from './constants.js';
 import type {RotationDegrees} from './types.js';
 
 const PACKAGE_NAME = 'react-fast-pdf';
 
-export {PDFPreviewer};
+export {PDFPreviewer, ROTATION_DEGREES};
 export type {RotationDegrees};
 
 export default {
